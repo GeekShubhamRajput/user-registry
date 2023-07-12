f28276a02b541a3e69716957be205ef4a17cfb56
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-07-12 23:02:17 +0530

    Enrollment module

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -1,2 +1,13 @@
 module EnrollmentsHelper
+  def registry_options
+    options_for_select(Registry.all.map { |r| [r.name, r.id] })
+  end
+
+  def participant_options
+    options_for_select(Participant.all.map { |r| [r.name, r.id] })
+  end
+
+  def coordinator_options
+    options_for_select(Coordinator.all.map { |r| [r.name, r.id] })
+  end
 end
