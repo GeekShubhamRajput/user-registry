576524eda60252a53c5af2209da52f4396aff624
Shubham Rajput <rajput.shubh1993@gmail.com>
2023-07-25 19:35:00 +0530

    Add enrollment index page

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -10,4 +10,8 @@ module EnrollmentsHelper
   def coordinator_options
     options_for_select(Coordinator.all.map { |r| [r.name, r.id] })
   end
+
+  def contact_method_options
+    options_for_select([['Phone', 'contact_via_phone'], ['Email', 'contact_via_email']])
+  end
 end
